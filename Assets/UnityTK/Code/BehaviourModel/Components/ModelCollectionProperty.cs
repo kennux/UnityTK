17344487c9d4e0e31b1dc979108c07121558f335
KennuX <kennux@ymail.com>
2018-03-29 19:16:11 +0200

    Removed collection property setter, added collection property removal handlers. Changed model collection property to implement ICollection rather than IEnumerable

--- Assets/UnityTK/Code/BehaviourModel/Components/ModelCollectionProperty.cs
+++ Assets/UnityTK/Code/BehaviourModel/Components/ModelCollectionProperty.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 namespace UnityTK.BehaviourModel
 {
@@ -10,7 +11,7 @@ namespace UnityTK.BehaviourModel
     /// Setters can consume objects from the set call in order to claim them being set on themselves.
     /// </summary>
     /// <typeparam name="T"></typeparam>
-    public class ModelCollectionProperty<T> : IEnumerable<T>
+    public class ModelCollectionProperty<T> : ICollection<T>
 	{
         /// <summary>
         /// Internal enumerator used for enumerating over a set of objects.
@@ -85,17 +86,18 @@ namespace UnityTK.BehaviourModel
         public delegate ICollection<T> Getter();
 
         /// <summary>
-        /// Set handler for <see cref="onSetValue"/>.
-        /// </summary>
-        /// <param name="queue">A queue of inserted elements, can dequeues consome the object for subsequent propagation calls</param>
-        public delegate void Setter(Queue<T> queue);
-
-        /// <summary>
         /// Insertion handler that can be used to handle object insertions.
         /// </summary>
         /// <param name="obj">The object to insert</param>
         /// <returns>Whether or not the object could be inserted</returns>
-        public delegate bool Inserter(T obj);
+        public delegate bool InsertHandler(T obj);
+
+        /// <summary>
+        /// Removal handler that can be used to handle object removals.
+        /// </summary>
+        /// <param name="obj">The object to remove from this collection property</param>
+        /// <returns>Whether or not the object could be removed</returns>
+        public delegate bool RemovalHandler(T obj);
 
         /// <summary>
         /// The getters bound to this property.
@@ -105,31 +107,37 @@ namespace UnityTK.BehaviourModel
         /// <summary>
         /// List of insertion handlers.
         /// </summary>
-        private List<Inserter> inserters = new List<Inserter>();
+        private List<InsertHandler> insertionHandlers = new List<InsertHandler>();
 
         /// <summary>
-        /// The setters bound to this property.
-        /// The parameter of this event is a queue that contains all elements that were set using <see cref="Set(ICollection{T})"/>.
-        /// 
-        /// Handlers of this event can dequeue elements from the queue to avoid next calls taking the same elements.
+        /// List of removal handlers.
         /// </summary>
-        public event Setter onSetValue;
+        private List<RemovalHandler> removalHandlers = new List<RemovalHandler>();
 
         /// <summary>
         /// Registers a collection getter that is being used in <see cref="Get"/>.
         /// </summary>
-		public void RegisterGetter(Getter getter)
+        public void RegisterGetter(Getter getter)
 		{
             this.getters.Add(getter);
 		}
 
         /// <summary>
         /// Registers an insertion handler.
-        /// <see cref="Inserter"/>
+        /// <see cref="InsertHandler"/>
         /// </summary>
-        public void RegisterInsertHandler(Inserter inserter)
+        public void RegisterInsertHandler(InsertHandler inserter)
         {
-            this.inserters.Add(inserter);
+            this.insertionHandlers.Add(inserter);
+        }
+
+        /// <summary>
+        /// Registers a removal handler.
+        /// <see cref="RemovalHandler"/>
+        /// </summary>
+        public void RegisterRemovalHandler(RemovalHandler handler)
+        {
+            this.removalHandlers.Add(handler);
         }
 
         /// <summary>
@@ -153,43 +161,38 @@ namespace UnityTK.BehaviourModel
 		}
 
         /// <summary>
-        /// Sets the specified collection to this property.
-        /// This will invoke the <see cref="onSetValue"/> event.
+        /// Removes the specified object from this collection property.
+        /// <see cref="RemovalHandler"/>, <see cref="RegisterRemovalHandler(RemovalHandler)"/>
         /// </summary>
-        /// <param name="col">The collection to set</param>
-		public void Set(ICollection<T> col)
-		{
-			if (this.onSetValue == null)
-				Debug.LogWarning("Tried setting collection property with no setters bound!");
-			else
+        /// <param name="obj">The object to be removed</param>
+        /// <returns>Whether or not the specified object could be removed.</returns>
+        public bool Remove(T obj)
+        {
+            if (this.removalHandlers.Count == 0)
+                Debug.LogWarning("Tried removing value from model collection property without removal handlers!");
+            else
             {
-                // Put col into a queue for implementor consumation
-                Queue<T> queue = QueuePool<T>.Get();
-
-                foreach (var obj in col)
-                    queue.Enqueue(obj);
-
-                // Pass queue to listeners
-                this.onSetValue(queue);
-
-                // Return queue
-                QueuePool<T>.Return(queue);
+                foreach (var removalHandler in this.removalHandlers)
+                    if (removalHandler.Invoke(obj))
+                        return true;
             }
-		}
+
+            return false;
+        }
 
         /// <summary>
         /// Inserts the specified object into this colletion.
-        /// This operation is only supported if <see cref="RegisterInsertHandler(Inserter)"/> inserters are registered.
+        /// This operation is only supported if <see cref="RegisterInsertHandler(InsertHandler)"/> inserters are registered.
         /// </summary>
         /// <returns>Whether or not the item could be inserted.</returns>
         public bool Insert(T obj)
         {
-            if (this.inserters.Count == 0)
+            if (this.insertionHandlers.Count == 0)
                 Debug.LogWarning("Tried inserting value into model collection property without inserters bound!");
             else
             {
-                foreach (var inserter in this.inserters)
-                    if (inserter.Invoke(obj))
+                foreach (var insertionHandler in this.insertionHandlers)
+                    if (insertionHandler.Invoke(obj))
                         return true; // insertion successfull!
             }
 
@@ -197,7 +200,64 @@ namespace UnityTK.BehaviourModel
             return false;
         }
 
-        public IEnumerator<T> GetEnumerator()
+        #region Interface implementation
+
+        int ICollection<T>.Count
+        {
+            get
+            {
+                int c = 0;
+                foreach (var getter in this.getters)
+                    c += getter.Invoke().Count;
+
+                return c;
+            }
+        }
+
+        bool ICollection<T>.IsReadOnly
+        {
+            get { return this.insertionHandlers.Count == 0 && this.removalHandlers.Count == 0; }
+        }
+
+        void ICollection<T>.Add(T item)
+        {
+            Insert(item);
+        }
+
+        void ICollection<T>.Clear()
+        {
+            List<T> lst = ListPool<T>.Get();
+            lst.AddRange(this);
+
+            for (int i = 0; i < lst.Count; i++)
+                if (!Remove(lst[i]))
+                    Debug.LogError("Could not remove object " + lst[i] + " from model collection property tho it was in there?!");
+
+            ListPool<T>.Return(lst);
+        }
+
+        bool ICollection<T>.Contains(T item)
+        {
+            // TODO: allow user defined equality comparator
+            foreach (var val in this)
+                if (EqualityComparer<T>.Default.Equals(val, item))
+                    return true;
+
+            return false;
+        }
+
+        void ICollection<T>.CopyTo(T[] array, int arrayIndex)
+        {
+            int ptr = arrayIndex;
+
+            foreach (var obj in this)
+            {
+                array[ptr] = obj;
+                ptr++;
+            }
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
         {
             return Get();
         }
@@ -206,5 +266,6 @@ namespace UnityTK.BehaviourModel
         {
             return Get();
         }
+        #endregion
     }
 }
--- Assets/UnityTK/Code/BehaviourModel/Editor/Test/BehaviourModelComponentsTest.cs
+++ Assets/UnityTK/Code/BehaviourModel/Editor/Test/BehaviourModelComponentsTest.cs
@@ -80,44 +80,71 @@ namespace UnityTK.BehaviourModel.Editor.Test
             l2.Clear();
 
             // Set handlers
-            collectionProperty.onSetValue += (queue) =>
+            collectionProperty.RegisterInsertHandler((i) =>
             {
-                if (queue.Count > 0)
-                    l1.Add(queue.Dequeue());
-            };
-            collectionProperty.onSetValue += (queue) =>
+                if (i != 123)
+                    return false;
+
+                l1.Add(i);
+                return true;
+            });
+            collectionProperty.RegisterInsertHandler((i) =>
             {
-                if (queue.Count > 0)
-                    l2.Add(queue.Dequeue());
-            };
+                l2.Add(i);
+                return true;
+            });
 
             // Set data
-            collectionProperty.Set(new int[] { 123, 321 });
+            collectionProperty.Insert(123);
+            collectionProperty.Insert(321);
 
             // Assert set data
             Assert.AreEqual(123, l1[0]);
             Assert.AreEqual(321, l2[0]);
 
-            // Append insertion handlers
-            collectionProperty.RegisterInsertHandler((obj) =>
+            // Append removal handlers
+            collectionProperty.RegisterRemovalHandler((i) =>
             {
+                if (l1.Contains(i))
+                {
+                    l1.Remove(i);
+                    return true;
+                }
+                else if (l2.Contains(i))
+                {
+                    l2.Remove(i);
+                    return true;
+                }
+
                 return false;
             });
-            collectionProperty.RegisterInsertHandler((obj) =>
-            {
-                if (obj != 456)
-                    return false;
 
-                l2.Add(obj);
-                return true;
-            });
+            Assert.IsFalse(collectionProperty.Remove(456));
+            Assert.IsTrue(collectionProperty.Remove(123));
+            Assert.AreEqual(0, l1.Count);
 
-            // Insert
-            Assert.IsTrue(collectionProperty.Insert(456));
-            Assert.IsFalse(collectionProperty.Insert(1234));
+            // Test ICollection methods
+            collectionProperty = new ModelCollectionProperty<int>();
+            l1 = new List<int>();
+            collectionProperty.RegisterGetter(() => l1);
+            collectionProperty.RegisterInsertHandler((i) => { l1.Add(i); return true; });
+            collectionProperty.RegisterRemovalHandler((i) => l1.Remove(i));
 
-            // Assert insertion worked
-            Assert.AreEqual(456, l2[1]);
+            ICollection<int> collection = collectionProperty;
+            collection.Add(123);
+            collection.Add(321);
+
+            Assert.IsTrue(collection.Contains(321));
+            Assert.AreEqual(2, collection.Count);
+            Assert.IsFalse(collection.IsReadOnly);
+            Assert.IsTrue(((ICollection<int>)new ModelCollectionProperty<int>()).IsReadOnly);
+
+            int[] test = new int[2];
+            collection.CopyTo(test, 0);
+            CollectionAssert.AreEqual(collection, test);
+
+            collection.Clear();
+            Assert.AreEqual(0, collection.Count);
         }
 
         [Test]
