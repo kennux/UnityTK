166f590025c1eb38c89754f9d2f0de1d4f47f942
KennuX <kennux@ymail.com>
2018-07-21 23:23:50 +0200

    Refactored ordered event Register to Bind. Added ordered event unbinding

--- Assets/UnityTK/Code/EditorCode/Tests/Utility/OrderedEventTests.cs
+++ Assets/UnityTK/Code/EditorCode/Tests/Utility/OrderedEventTests.cs
@@ -7,37 +7,20 @@ namespace UnityTK.Test
 {
     public class OrderedEventTests
     {
+        private int firstCalled;
+        private int secondCalled;
+
         [Test]
         public void OrderedEventTest()
         {
             // Arrange
             OrderedEvent evt = new OrderedEvent();
             int q1 = 100, q2 = 200;
-            int firstCalled = -1, secondCalled = -1;
+            firstCalled = secondCalled = -1;
 
-            evt.Register(() =>
-            {
-                if (firstCalled == -1)
-                {
-                    firstCalled = 1;
-                }
-                else if (secondCalled == -1)
-                {
-                    secondCalled = 1;
-                }
-            }, q1);
+            evt.Bind(this.FirstCall, q1);
 
-            evt.Register(() =>
-            {
-                if (firstCalled == -1)
-                {
-                    firstCalled = 2;
-                }
-                else if (secondCalled == -1)
-                {
-                    secondCalled = 2;
-                }
-            }, q2);
+            evt.Bind(this.SecondCall, q2);
 
             // Act
             evt.Invoke();
@@ -45,6 +28,41 @@ namespace UnityTK.Test
             // Assert
             Assert.AreEqual(1, firstCalled);
             Assert.AreEqual(2, secondCalled);
+
+            // Test unbind
+            firstCalled = secondCalled = -1;
+
+            // Act
+            evt.Unbind(SecondCall);
+            evt.Invoke();
+
+            // Assert
+            Assert.AreEqual(1, firstCalled);
+            Assert.AreEqual(-1, secondCalled);
+        }
+
+        private void FirstCall()
+        {
+            if (firstCalled == -1)
+            {
+                firstCalled = 1;
+            }
+            else if (secondCalled == -1)
+            {
+                secondCalled = 1;
+            }
+        }
+
+        private void SecondCall()
+        {
+            if (firstCalled == -1)
+            {
+                firstCalled = 2;
+            }
+            else if (secondCalled == -1)
+            {
+                secondCalled = 2;
+            }
         }
     }
 }
