867dee6522a11c5ed81e8ef4c792d2f60553e80d
Kenneth <kennux@ymail.com>
2019-09-19 14:48:33 +0200

    Another latency fix, databinding roots now can choose whether to be updated in Update(), LateUpdate() or FixedUpdate().

--- Assets/UnityTK/Code/DataBinding/DataBindingReflectionRoot.cs
+++ Assets/UnityTK/Code/DataBinding/DataBindingReflectionRoot.cs
@@ -5,12 +5,26 @@ using UnityEngine;
 
 namespace UnityTK.DataBinding
 {
+	public enum DataBindingUpdateMode
+	{
+		Update,
+		LateUpdate,
+		FixedUpdate
+	}
+
 	/// <summary>
 	/// Abstract implementation for databinding roots which work by reflection.
 	/// Used in <see cref="DataBindingRoot"/> and <see cref="DataBindingScriptedRoot"/>
 	/// </summary>
 	public abstract class DataBindingReflectionRoot : DataBindingReflectionNode
 	{
+		/// <summary>
+		/// The unity message used to run databinding updates.
+		/// The standard value is LateUpdate, since most of the time gameobjects are being enabled / disabled by other gameobjects in Update(),
+		/// In order to still get updated after being enabled, LateUpdate is used as it is fired right after Update and unity won't fire Update on objects enabled from an Update call.
+		/// </summary>
+		public DataBindingUpdateMode updateMode = DataBindingUpdateMode.LateUpdate;
+
 		/// <summary>
 		/// The update framerate.
 		/// </summary>
@@ -49,7 +63,7 @@ namespace UnityTK.DataBinding
 			this.justEnabled = true;
 		}
 
-		public void Update()
+		protected virtual void FrameUpdate()
 		{
 			this._time += Time.deltaTime;
 
@@ -59,6 +73,24 @@ namespace UnityTK.DataBinding
 				this._time = 0;
 				this.justEnabled = false;
 			}
+		}
+
+		public void Update()
+		{
+			if (updateMode == DataBindingUpdateMode.Update)
+				FrameUpdate();
+		}
+
+		public void FixedUpdate()
+		{
+			if (updateMode == DataBindingUpdateMode.FixedUpdate)
+				FrameUpdate();
+		}
+
+		public void LateUpdate()
+		{
+			if (updateMode == DataBindingUpdateMode.LateUpdate)
+				FrameUpdate();
 		}
 	}
 }
